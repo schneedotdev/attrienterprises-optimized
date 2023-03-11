fc3c794a83a01798d43b02cf4012fede28267044
brianschnee <brian.a.schnee@gmail.com>
2023-03-11 12:48:22 -0500

    clientside js

---
+++ public/js/dropdown.js
@@ -0,0 +1,20 @@
+// Dropdown functionality// listen for a click anywhere in the document
+  document.addEventListener('click', (e) => {
+    // check to see if dropdown button was clicked
+    const isDropdownButton = e.target.matches('[data-dropdown-button]');
+
+    // if the dropdown was clicked, toggle the active class and display the dropdown (css will apply the styling to make the dropdown appear)
+    if (isDropdownButton) {
+      let currentDropdown = e.target.closest('[data-dropdown]');
+      currentDropdown.classList.toggle('active');
+    } else {
+      // if the click existed within the dropdown, ignore the click
+      if (e.target.closest('[data-dropdown]') != null) return;
+
+      // if the active class isnt applied to the dropdown yet, ignore, otherwise remove the active class
+      const dropdown = document.querySelector('[data-dropdown].active');
+
+      if (dropdown !== null)
+        dropdown.classList.remove('active');
+    }
+  });
