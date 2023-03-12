d0abb53e3ff5f03660b98190b9868a3b9d401fb2
brianschnee <brian.a.schnee@gmail.com>
2023-03-11 22:19:51 -0500

    react -> preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [preact()],
   output: "server",
   adapter: netlify()
 });
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'preact/hooks'
 import Image from './Image'
 
 type ProjectProps = {
